c8458755b388cc8ba6ed01d7bfdbf71747466c75
Sriharsha Samana <sriharsha.samana@gmail.com>
2019-11-16 09:46:13 -0500

    added image background to user cards

--- src/components/User.vue
+++ src/components/User.vue
@@ -6,61 +6,33 @@
       max-width="400"
       type="card-avatar"
     ></v-skeleton-loader>
-    <v-card v-else class="ma-1" max-width="400">
-      <v-card-title>
-        <v-row v-if="user.activity == 'sleep'" class="pa-2">
-          <v-col cols="3">
-            <v-icon color="primary" large left>fas fa-bed</v-icon>
-          </v-col>
-          <v-col cols="6">
-            <span class="subheading font-weight-light">Sleeping now..</span>
-          </v-col>
-          <v-col cols="3">
-            <span class="overline">{{ user.time }}</span>
-          </v-col>
-        </v-row>
-        <v-row v-if="user.activity == 'breakfast'" class="pa-2">
-          <v-col cols="3">
-            <v-icon color="primary" large left>fas fa-utensils</v-icon>
-          </v-col>
-          <v-col cols="6">
-            <span class="subheading font-weight-light">Eating...</span>
-          </v-col>
-          <v-col cols="3">
-            <span class="overline">{{ user.time }}</span>
-          </v-col>
-        </v-row>
-        <v-row v-if="user.activity == 'work'" class="pa-2">
-          <v-col cols="3">
-            <v-icon color="primary" large left>fas fa-business-time</v-icon>
-          </v-col>
-          <v-col cols="6">
-            <span class="subheading font-weight-light">Busy now..</span>
-          </v-col>
-          <v-col cols="3">
-            <span class="overline">{{ user.time }}</span>
-          </v-col>
-        </v-row>
-        <v-row v-if="user.activity == 'free'" class="pa-2">
-          <v-col cols="3">
-            <v-icon color="primary" large left>fas fa-umbrella-beach</v-icon>
-          </v-col>
-          <v-col cols="6">
-            <span class="subheading font-weight-light">Available now..</span>
-          </v-col>
-          <v-col cols="3">
-            <span class="overline">{{ user.time }}</span>
-          </v-col>
-        </v-row>
-      </v-card-title>
-
+    <v-card v-else class="ma-1" max-width="400" img>
+      <v-img
+        height="200px"
+        :src="activityPhoto"
+        class="white--text"
+        gradient="to top right, rgba(100,115,201,.33), rgba(25,32,72,.7)"
+      >
+        <v-card-title>
+          <v-row>
+            <v-col cols="9">
+              <span class="subheading font-weight-thin">{{
+                activityHeading
+              }}</span>
+            </v-col>
+            <v-col cols="3">
+              <span class="caption">{{ user.time }}</span>
+            </v-col>
+          </v-row>
+        </v-card-title>
+      </v-img>
       <v-card-text class="subheading font-weight-bold">{{
-        user.status
+        statusText
       }}</v-card-text>
 
       <v-card-actions>
         <v-list-item class="grow">
-          <v-list-item-avatar color="grey darken-3">
+          <v-list-item-avatar size="80" color="grey darken-3">
             <v-img :src="user.photo" class="elevation-6"></v-img>
           </v-list-item-avatar>
 
@@ -90,6 +62,38 @@ export default {
   data() {
     return {
       loading: true
+    }
+  },
+  computed: {
+    activityIcon() {
+      if (this.user.activity === 'sleep') return 'fas fa-bed'
+      else if (this.user.activity === 'breakfast') return 'fas fa-utensils'
+      else if (this.user.activity === 'work') return 'fas fa-business-time'
+      else if (this.user.activity === 'free') return 'fas fa-umbrella-beach'
+      else return ''
+    },
+    activityPhoto() {
+      if (this.user.activity === 'sleep')
+        return 'https://media.tenor.com/images/e498a53565bac7f47dd901c0676ccc53/tenor.gif'
+      else if (this.user.activity === 'breakfast') return 'https://i.chzbgr.com/full/8191306240/h73FB4A28/the-only-way-to-eat-a-burger'
+      else if (this.user.activity === 'work') return 'https://media.tenor.com/images/c18c487d770dbaef16ea7521cfe4f7d2/tenor.gif'
+      else if (this.user.activity === 'free') return 'https://media.tenor.com/images/86002300e062cd49c4e5eac9748f7546/tenor.gif'
+      else return ''
+    },
+    activityHeading() {
+      if (this.user.activity === 'sleep') return 'Sleeping now..'
+      else if (this.user.activity === 'breakfast') return 'Eating...'
+      else if (this.user.activity === 'work') return 'Busy now..'
+      else if (this.user.activity === 'free') return 'Available now..'
+      else return ''
+    },
+    statusText() {
+      if (this.user.status) return this.user.status
+      else if (this.user.activity === 'sleep') return 'Sleeping now..'
+      else if (this.user.activity === 'breakfast') return 'Eating...'
+      else if (this.user.activity === 'work') return 'Busy now..'
+      else if (this.user.activity === 'free') return 'Available now..'
+      else return ''
     }
   },
   mounted() {
