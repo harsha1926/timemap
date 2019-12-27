3224c74e6cd1dcbdc5cf8395d6af1ba8e5f9c439
Sriharsha Samana <sriharsha.samana@gmail.com>
2019-12-26 21:33:53 -0500

    Update Schedule.vue

--- src/components/schedule/Schedule.vue
+++ src/components/schedule/Schedule.vue
@@ -1,146 +1,122 @@
 <template>
-  <v-card class="mx-auto" max-width="400">
-    <v-card-text class="py-0">
-      <v-timeline align-top dense>
-        <v-timeline-item color="pink" small>
-          <v-row class="pt-1" justify="space-between">
-            <v-col cols="4">
-              <strong>6am</strong>
-            </v-col>
-            <v-col>
-              <strong>Good Morning!</strong>
-            </v-col>
-            <v-col>
-              <v-avatar>
-                <v-img
-                  src="https://media1.tenor.com/images/93a8248a7fad2e5d6d4294b3186dd969/tenor.gif?itemid=6101414"
-                >
-                  <template v-slot:placeholder>
-                    <v-row
-                      class="fill-height ma-0"
-                      align="center"
-                      justify="center"
-                    >
-                      <v-progress-circular
-                        indeterminate
-                        color="primary"
-                      ></v-progress-circular>
-                    </v-row>
-                  </template>
-                </v-img>
-              </v-avatar>
-            </v-col>
-          </v-row>
-        </v-timeline-item>
+  <v-container fluid>
+    <v-tabs v-model="tab" grow>
+      <v-tab :key="0">Weekday</v-tab>
+      <v-tab :key="1">Weekend</v-tab>
+    </v-tabs>
 
-        <v-timeline-item color="teal lighten-3" small>
-          <v-row class="pt-1">
-            <v-col cols="4">
-              <strong>7-7:30am</strong>
-            </v-col>
-            <v-col>
-              <strong>Breakfast</strong>
-            </v-col>
-            <v-col>
-              <v-avatar>
-                <v-img
-                  src="https://media1.tenor.com/images/c437f119f41c1753b322820fa3dc1c5a/tenor.gif?itemid=4298967"
-                ></v-img>
-              </v-avatar>
-            </v-col>
-          </v-row>
-        </v-timeline-item>
+    <v-tabs-items v-model="tab">
+      <v-tab-item :key="0">
+        <v-timeline align-top dense v-if="schedule && schedule.weekday">
+          <v-timeline-item color="primary" small>
+            <v-row class="pt-1" justify="space-between">
+              <v-col cols="4">
+                <strong>{{ formatTime(weekdayStartTime) }}</strong>
+              </v-col>
+              <v-col>
+                <strong>Good Morning!</strong>
+              </v-col>
+            </v-row>
+          </v-timeline-item>
 
-        <v-timeline-item color="teal lighten-3" small>
-          <v-row class="pt-1">
-            <v-col cols="4">
-              <strong>9-11:59am</strong>
-            </v-col>
-            <v-col>
-              <strong>Work</strong>
-            </v-col>
-            <v-col>
-              <v-avatar>
-                <v-img
-                  src="https://media1.tenor.com/images/00d30d37568ad62b5f5dc094fba12781/tenor.gif?itemid=15464155"
-                ></v-img>
-              </v-avatar>
-            </v-col>
-          </v-row>
-        </v-timeline-item>
+          <v-timeline-item
+            color="primary"
+            small
+            v-for="activity in weekdayRoutine"
+            :key="activity.id"
+          >
+            <v-row class="pt-1" justify="space-between">
+              <v-col cols="4">
+                <strong>{{ formatTime(activity.startTime) }}</strong>
+              </v-col>
+              <v-col>
+                <strong>{{ activity.id }}</strong>
+              </v-col>
+            </v-row>
+          </v-timeline-item>
 
-        <v-timeline-item color="pink" small>
-          <v-row class="pt-1">
-            <v-col cols="4">
-              <strong>12-1pm</strong>
-            </v-col>
-            <v-col>
-              <strong>Lunch</strong>
-            </v-col>
-            <v-col>
-              <v-avatar>
-                <v-img
-                  src="https://media1.tenor.com/images/e3f1d2accba9926e18d8178846f69b5d/tenor.gif?itemid=12758265"
-                ></v-img>
-              </v-avatar>
-            </v-col>
-          </v-row>
-        </v-timeline-item>
-
-        <v-timeline-item color="teal lighten-3" small>
-          <v-row class="pt-1">
-            <v-col cols="4">
-              <strong>1-5pm</strong>
-            </v-col>
-            <v-col>
-              <strong>Work</strong>
-            </v-col>
-            <v-col>
-              <v-avatar>
-                <v-img
-                  src="https://media1.tenor.com/images/00d30d37568ad62b5f5dc094fba12781/tenor.gif?itemid=15464155"
-                ></v-img>
-              </v-avatar>
-            </v-col>
-          </v-row>
-        </v-timeline-item>
-
-        <v-timeline-item color="teal lighten-3" small>
-          <v-row class="pt-1">
-            <v-col cols="4">
-              <strong>7pm</strong>
-            </v-col>
-            <v-col>
-              <strong>Dinner</strong>
-            </v-col>
-            <v-col>
-              <v-avatar>
-                <v-img
-                  src="https://media1.tenor.com/images/eea8fd566ed01499a174af5c14a7a2b7/tenor.gif?itemid=3975093"
-                ></v-img>
-              </v-avatar>
-            </v-col>
-          </v-row>
-        </v-timeline-item>
-
-        <v-timeline-item color="teal lighten-3" small>
-          <v-row class="pt-1">
-            <v-col cols="4">
-              <strong>9pm</strong>
-            </v-col>
-            <v-col>
-              <strong>Sleep</strong>
-            </v-col>
-            <v-col>
-              <v-avatar>
-                <v-img
-                  src="https://media1.tenor.com/images/777f97d45fa800c82f6162944d7070ea/tenor.gif?itemid=8578132"
-                ></v-img>
-              </v-avatar>
-            </v-col>
-          </v-row>
-        </v-timeline-item>
-      </v-timeline>
-    </v-card-text>
-  </v-card>
+          <v-timeline-item color="primary" small>
+            <v-row class="pt-1" justify="space-between">
+              <v-col cols="4">
+                <strong>{{ formatTime(weekdayEndTime) }}</strong>
+              </v-col>
+              <v-col>
+                <strong>Good Night!</strong>
+              </v-col>
+            </v-row>
+          </v-timeline-item>
+        </v-timeline>
+      </v-tab-item>
+    </v-tabs-items>
+  </v-container>
 </template>
+<script>
+import { firebaseDB } from '@/services/firebaseInit.js'
+import { mapGetters } from 'vuex'
+import moment from 'moment'
+export default {
+  data() {
+    return {
+      schedule: null,
+      tab: null
+    }
+  },
+  computed: {
+    ...mapGetters('user', ['uid']),
+    weekdayStartTime: function() {
+      if (this.schedule && this.schedule.weekday)
+        return this.schedule.weekday.dayStartTime
+      else return null
+    },
+    weekdayEndTime: function() {
+      if (this.schedule && this.schedule.weekday)
+        return this.schedule.weekday.dayEndTime
+      else return null
+    },
+    weekdayRoutine: function() {
+      if (this.schedule && this.schedule.weekday) {
+        let activities = []
+        for (let activity in this.schedule.weekday.routine) {
+          activities.push({
+            id: activity,
+            title: this.schedule.weekday.routine[activity].title,
+            startTime: this.schedule.weekday.routine[activity].startTime,
+            duration: this.schedule.weekday.routine[activity].duration
+          })
+        }
+        activities = activities.sort((a, b) => {
+          let aStartTime = moment(a.startTime, 'HH:mm:ss')
+          let bStartTime = moment(b.startTime, 'HH:mm:ss')
+          if (aStartTime.isAfter(bStartTime)) {
+            return 1
+          } else if (aStartTime.isBefore(bStartTime)) {
+            return -1
+          } else {
+            return 0
+          }
+        })
+        return activities
+      } else return []
+    }
+  },
+  mounted: function() {
+    let vm = this
+    firebaseDB.ref('schedule/' + vm.uid).once('value', function(data) {
+      if (data.val()) vm.schedule = data.val()
+      else
+        firebaseDB.ref('schedule/default').once('value', function(data) {
+          vm.schedule = data.val()
+        })
+    })
+  },
+  methods: {
+    formatTime(time) {
+      if (time) {
+        return moment(time, 'HH:mm:ss').format('hh:mm a')
+      } else {
+        return ''
+      }
+    }
+  }
+}
+</script>
