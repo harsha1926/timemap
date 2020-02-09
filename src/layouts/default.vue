c2a5a30ba1521c7a9422216cf035e1e73bad75b4
Sriharsha Samana <sriharsha.samana@gmail.com>
2020-02-09 10:42:25 -0500

    Update default.vue

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,39 +1,77 @@
 <template>
   <v-app id="watch">
     <v-app-bar :dense="$vuetify.breakpoint.xsOnly" app tile flat>
-      <v-row align="center">
-        <v-flex @click="$router.push('/')" class="customPointer text-left">
-          <v-avatar size="35">
-            <v-img :src="iconURL"></v-img>
-          </v-avatar>
-          <span class="ml-2 pt-2 appTitleFont primary--text">Watch</span>
-        </v-flex>
-        <v-flex class="text-right">
-          <v-row justify="end" align="center">
-            <span v-if="displayName" class="mr-5 d-none d-sm-block"
-              >Hello {{ displayName }}</span
-            >
-            <v-avatar
-              @click="$router.push('/account')"
-              v-if="photoURL"
-              class="mr-5 customPointer"
-              size="35"
-            >
-              <v-img :src="photoURL"></v-img>
-            </v-avatar>
-          </v-row>
-        </v-flex>
-      </v-row>
+      <v-container fluid class="pa-0 ma-0">
+        <v-row align="center" class="mb-1">
+          <v-flex @click="$router.push('/')" class="customPointer text-left">
+            <v-row justify="start" align="center" class="ml-2">
+              <v-avatar size="35">
+                <v-img :src="iconURL"></v-img>
+              </v-avatar>
+              <span class="ml-3 appTitleFont primary--text">Watch</span>
+            </v-row>
+          </v-flex>
+          <v-flex class="text-right">
+            <v-row justify="end" align="center">
+              <span v-if="displayName" class="mr-5 d-none d-sm-block">Hello {{ displayName }}</span>
+              <v-avatar
+                @click="$router.push('/account')"
+                v-if="photoURL"
+                class="mr-5 customPointer"
+                size="35"
+              >
+                <v-img :src="photoURL"></v-img>
+              </v-avatar>
+            </v-row>
+          </v-flex>
+        </v-row>
+        <v-row>
+          <div style="background-color:#D8D8D8; height: 1px; width:100%;"></div>
+        </v-row>
+      </v-container>
     </v-app-bar>
     <v-content>
-      <v-divider></v-divider>
       <v-container fluid class="mt-0 pt-0">
         <login v-if="!uid"></login>
         <verify-phone v-else-if="uid && !phoneNumber"></verify-phone>
         <nuxt v-else-if="uid && phoneNumber"></nuxt>
       </v-container>
     </v-content>
-    <v-bottom-navigation
+
+    <v-footer app>
+      <v-container fluid class="pa-0 ma-0">
+        <v-row>
+          <div style="background-color:#D8D8D8; height: 1px; width:100%;"></div>
+        </v-row>
+        <v-row row align="center" justify="space-between" class="mt-1">
+          <v-flex class="text-center">
+            <v-btn color="primary" text to="/" value="home">
+              <v-icon>fas fa-eye</v-icon>
+            </v-btn>
+          </v-flex>
+
+          <v-flex class="text-center">
+            <v-btn color="primary" text to="/findFriends" value="findFriends">
+              <v-icon>fas fa-search</v-icon>
+            </v-btn>
+          </v-flex>
+
+          <v-flex class="text-center">
+            <v-btn color="primary" text to="/schedule" value="schedule">
+              <v-icon>far fa-calendar-check</v-icon>
+            </v-btn>
+          </v-flex>
+
+          <v-flex class="text-center">
+            <v-btn color="primary" text to="/posts" value="posts">
+              <v-icon>fas fa-angle-double-up</v-icon>
+            </v-btn>
+          </v-flex>
+        </v-row>
+      </v-container>
+    </v-footer>
+
+    <!-- <v-bottom-navigation
       v-if="uid"
       :height="$vuetify.breakpoint.xsOnly ? 45 : 65"
       v-model="activeTab"
@@ -53,7 +91,7 @@
       <v-btn to="/posts" value="posts">
         <v-icon>fas fa-angle-double-up</v-icon>
       </v-btn>
-    </v-bottom-navigation>
+    </v-bottom-navigation>-->
   </v-app>
 </template>
 <script>
