c73473b15fba2fd59c4e8c8ef0406b59f8246ddc
Sriharsha Samana <sriharsha.samana@gmail.com>
2020-02-09 20:10:07 -0500

    pwa

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,39 @@
-const functions = require("firebase-functions");
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
 
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
+var {google} = require('googleapis');
+var people = google.people('v1');
 
-exports.getApiKey = functions.https.onRequest((request, response) => {
-  console.log(request.query.key);
-  response.send("Hello from Firebase!");
+exports.contacts = functions.https.onRequest((request, response) => {
+  admin.database().ref('/settings/contacts/credentials/serviceAccount').once("value", function(data) {
+    var authClient = new google.auth.JWT(
+      data.child('clientEmail').val(),
+      null,
+      data.child('privateKey').val(),
+      ['https://www.googleapis.com/auth/contacts'],
+      null
+    );
+
+    authClient.authorize(function (err, tokens) {
+      if (err) {
+        console.error(err);
+        response.end();
+        return;
+      }
+
+      // Make an authorized request to list contacts.
+      people.people.connections.list({auth: authClient, resourceName: 'people/me'}, function(err, resp) {
+        if (err) {
+          console.error(err);
+          response.end();
+          return;
+        }
+
+        console.log("Success");
+        console.log(resp);
+        response.send(resp);
+      });
+    });
+  });
 });
--- src/nuxt.config.js
+++ src/nuxt.config.js
@@ -37,7 +37,7 @@ export default {
   loadingIndicator: {
     name: 'folding-cube',
     color: '#e67e22',
-    background: 'white'
+    background: '#ffffff'
   },
   /*
    ** Global CSS
@@ -90,53 +90,12 @@ export default {
     manifest: {
       name: 'Watch',
       short_name: 'Watch',
-      theme_color: '#e67e22',
-      background_color: '#e67e22',
+      lang: 'en',
       display: 'standalone',
+      theme_color: '#e67e22',
+      background_color: '#ffffff',
       orientation: 'portrait',
-      start_url: '/',
-      icons: [
-        {
-          src: 'icon-72x72.png',
-          sizes: '72x72',
-          type: 'image/png'
-        },
-        {
-          src: 'icon-96x96.png',
-          sizes: '96x96',
-          type: 'image/png'
-        },
-        {
-          src: 'icon-128x128.png',
-          sizes: '128x128',
-          type: 'image/png'
-        },
-        {
-          src: 'icon-144x144.png',
-          sizes: '144x144',
-          type: 'image/png'
-        },
-        {
-          src: 'icon-152x152.png',
-          sizes: '152x152',
-          type: 'image/png'
-        },
-        {
-          src: 'icon-192x192.png',
-          sizes: '192x192',
-          type: 'image/png'
-        },
-        {
-          src: 'icon-384x384.png',
-          sizes: '384x384',
-          type: 'image/png'
-        },
-        {
-          src: 'icon-512x512.png',
-          sizes: '512x512',
-          type: 'image/png'
-        }
-      ]
+      start_url: '/'
     }
   },
   /*
